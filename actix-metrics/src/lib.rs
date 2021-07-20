999d48d18acc25b6a29fab2d6de1c5a35a65c757
Nerixyz <nero.9@hotmail.de>
2021-07-20 12:47:07 +0200

    feat: initial commit

---
+++ actix-metrics/src/lib.rs
@@ -0,0 +1,129 @@
+use actix_utils::future::{ok, Ready};
+use actix_web::{
+    dev::{Service, ServiceRequest, ServiceResponse, Transform},
+    Error,
+};
+use futures::ready;
+use std::{
+    future::Future,
+    pin::Pin,
+    rc::Rc,
+    task::{Context, Poll},
+    time::{Duration, Instant},
+};
+
+pub struct Metrics(Rc<Inner>);
+
+struct Inner {
+    service: String,
+}
+
+impl Metrics {
+    pub fn new<T: std::fmt::Display>(service: T) -> Self {
+        Self(Rc::new(Inner {
+            service: service.to_string(),
+        }))
+    }
+    pub fn register_metrics() {
+        metrics::register_histogram!(
+            "http_request_duration",
+            metrics::Unit::Milliseconds,
+            "Request processing duration"
+        );
+        metrics::register_counter!("http_requests", "Total requests");
+    }
+}
+
+impl<S> Transform<S, ServiceRequest> for Metrics
+where
+    S: Service<ServiceRequest, Response = ServiceResponse, Error = Error> + 'static,
+    S::Future: 'static,
+{
+    type Response = ServiceResponse;
+    type Error = Error;
+    type Transform = MetricsMiddleware<S>;
+    type InitError = ();
+    type Future = Ready<Result<Self::Transform, Self::InitError>>;
+
+    fn new_transform(&self, service: S) -> Self::Future {
+        ok(MetricsMiddleware {
+            service,
+            inner: self.0.clone(),
+        })
+    }
+}
+
+pub struct MetricsMiddleware<S> {
+    inner: Rc<Inner>,
+    service: S,
+}
+
+impl<S> Service<ServiceRequest> for MetricsMiddleware<S>
+where
+    S: Service<ServiceRequest, Response = ServiceResponse, Error = Error> + 'static,
+    S::Future: 'static,
+{
+    type Response = ServiceResponse;
+    type Error = Error;
+    type Future = MetricsResponse<S>;
+
+    fn poll_ready(&self, ctx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        self.service.poll_ready(ctx)
+    }
+
+    fn call(&self, req: ServiceRequest) -> Self::Future {
+        let now = Instant::now();
+
+        MetricsResponse {
+            fut: self.service.call(req),
+            start: now,
+            service: Some(self.inner.service.clone()),
+        }
+    }
+}
+
+#[pin_project::pin_project]
+pub struct MetricsResponse<S>
+where
+    S: Service<ServiceRequest>,
+{
+    #[pin]
+    fut: S::Future,
+    start: Instant,
+    service: Option<String>,
+}
+
+impl<S> Future for MetricsResponse<S>
+where
+    S: Service<ServiceRequest, Response = ServiceResponse, Error = Error>,
+{
+    type Output = Result<ServiceResponse, Error>;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let this = self.project();
+
+        let res: ServiceResponse = match ready!(this.fut.poll(cx)) {
+            Ok(res) => res,
+            Err(e) => return Poll::Ready(Err(e)),
+        };
+
+        let duration = Instant::now()
+            .checked_duration_since(*this.start)
+            .unwrap_or_else(|| Duration::from_secs(0));
+        let service = this.service.take().unwrap();
+        metrics::histogram!(
+            "http_request_duration",
+            duration.as_millis() as f64,
+            "service" => service.clone(),
+            "status" => res.status().as_str().to_owned(),
+            "method" => res.request().method().as_str().to_owned()
+        );
+        metrics::increment_counter!("http_requests",
+            "service" => service,
+            "status" => res.status().as_str().to_owned(),
+            "method" => res.request().method().as_str().to_owned()
+        );
+
+        Poll::Ready(Ok(res))
+    }
+}
---
+++ errors/src/lib.rs
@@ -0,0 +1,71 @@
+pub mod json_error;
+pub mod redirect_error;
+#[cfg(feature = "sql-result")]
+pub mod sql;
+
+macro_rules! error_helper {
+    ($name:ident, $status:ident) => {
+        paste::paste! {
+            #[doc = "Helper function that wraps any error and generates a `" $status "` response."]
+            #[allow(non_snake_case)]
+            pub fn $name<T>(err: T) -> actix_web::Error
+            where
+            T: std::fmt::Debug + std::fmt::Display + serde::Serialize + 'static,
+            {
+                json_error::JsonError::new(err, actix_web::http::StatusCode::$status).into()
+            }
+        }
+    };
+}
+
+error_helper!(ErrorBadRequest, BAD_REQUEST);
+error_helper!(ErrorUnauthorized, UNAUTHORIZED);
+error_helper!(ErrorPaymentRequired, PAYMENT_REQUIRED);
+error_helper!(ErrorForbidden, FORBIDDEN);
+error_helper!(ErrorNotFound, NOT_FOUND);
+error_helper!(ErrorMethodNotAllowed, METHOD_NOT_ALLOWED);
+error_helper!(ErrorNotAcceptable, NOT_ACCEPTABLE);
+error_helper!(
+    ErrorProxyAuthenticationRequired,
+    PROXY_AUTHENTICATION_REQUIRED
+);
+error_helper!(ErrorRequestTimeout, REQUEST_TIMEOUT);
+error_helper!(ErrorConflict, CONFLICT);
+error_helper!(ErrorGone, GONE);
+error_helper!(ErrorLengthRequired, LENGTH_REQUIRED);
+error_helper!(ErrorPayloadTooLarge, PAYLOAD_TOO_LARGE);
+error_helper!(ErrorUriTooLong, URI_TOO_LONG);
+error_helper!(ErrorUnsupportedMediaType, UNSUPPORTED_MEDIA_TYPE);
+error_helper!(ErrorRangeNotSatisfiable, RANGE_NOT_SATISFIABLE);
+error_helper!(ErrorImATeapot, IM_A_TEAPOT);
+error_helper!(ErrorMisdirectedRequest, MISDIRECTED_REQUEST);
+error_helper!(ErrorUnprocessableEntity, UNPROCESSABLE_ENTITY);
+error_helper!(ErrorLocked, LOCKED);
+error_helper!(ErrorFailedDependency, FAILED_DEPENDENCY);
+error_helper!(ErrorUpgradeRequired, UPGRADE_REQUIRED);
+error_helper!(ErrorPreconditionFailed, PRECONDITION_FAILED);
+error_helper!(ErrorPreconditionRequired, PRECONDITION_REQUIRED);
+error_helper!(ErrorTooManyRequests, TOO_MANY_REQUESTS);
+error_helper!(
+    ErrorRequestHeaderFieldsTooLarge,
+    REQUEST_HEADER_FIELDS_TOO_LARGE
+);
+error_helper!(
+    ErrorUnavailableForLegalReasons,
+    UNAVAILABLE_FOR_LEGAL_REASONS
+);
+error_helper!(ErrorExpectationFailed, EXPECTATION_FAILED);
+error_helper!(ErrorInternalServerError, INTERNAL_SERVER_ERROR);
+error_helper!(ErrorNotImplemented, NOT_IMPLEMENTED);
+error_helper!(ErrorBadGateway, BAD_GATEWAY);
+error_helper!(ErrorServiceUnavailable, SERVICE_UNAVAILABLE);
+error_helper!(ErrorGatewayTimeout, GATEWAY_TIMEOUT);
+error_helper!(ErrorHttpVersionNotSupported, HTTP_VERSION_NOT_SUPPORTED);
+error_helper!(ErrorVariantAlsoNegotiates, VARIANT_ALSO_NEGOTIATES);
+error_helper!(ErrorInsufficientStorage, INSUFFICIENT_STORAGE);
+error_helper!(ErrorLoopDetected, LOOP_DETECTED);
+error_helper!(ErrorNotExtended, NOT_EXTENDED);
+error_helper!(
+    ErrorNetworkAuthenticationRequired,
+    NETWORK_AUTHENTICATION_REQUIRED
+);
